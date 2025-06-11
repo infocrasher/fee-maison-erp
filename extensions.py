6d152203dc81f3bd299a3b8423c9a1f32a9ea880
Sofiane <infocrasher@gmail.com>
2025-06-11 03:18:02 +0000

    🔄 A75904:18162-22025n3 3RP 11/06/2025 04:18:02ée 6AMi2162n - {11/06/2025 04:18ilenAM18e} [{11AM04:18e04:18i18e}]

--- extensions.py
+++ extensions.py
@@ -1,13 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_wtf.csrf import CSRFProtect
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
-migrate = Migrate()
 login = LoginManager()
-csrf = CSRFProtect()
-
-login.login_view = 'login'
-login.login_message_category = 'info'
-login.login_message = "Veuillez vous connecter pour accéder à cette page."
+migrate = Migrate()
