cce5ae13461af3d4bd80a1124ec89c8613d5e322
Sofiane <infocrasher@gmail.com>
2025-06-11 02:53:16 +0000

    🔄 A89503:53162-162025n3 3RP 11/06/2025 03:53:16ée 6AMi16162n - {11/06/2025 03:53ilenAM53e} [{11AM03:53e03:53i53e}]

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import func
+from extensions import db
 
 db = SQLAlchemy()
 
