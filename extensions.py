1e7adc04096ce65c8cc227499dbcf0f4833f0cb0
Sofiane <infocrasher@gmail.com>
2025-06-16 20:56:35 +0000

    🔄 A04921:56167-352025n1 1RP 16/06/2025 21:56:35ée 6PMi35167n - {16/06/2025 21:56ilenPM56e} [{16PM21:56e21:56i56e}]

--- extensions.py
+++ extensions.py
@@ -1,7 +1,21 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_migrate import Migrate
+"""
+Extensions Flask pour l'ERP Fée Maison
+Fichier: extensions.py
+Auteur: ERP Fée Maison
+"""
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
+
+# Initialisation des extensions Flask
 db = SQLAlchemy()
-login = LoginManager()
 migrate = Migrate()
+login_manager = LoginManager()  # ← CORRECTION : Instance nommée login_manager
+csrf = CSRFProtect()
+
+# Configuration du LoginManager
+login_manager.login_view = 'auth.login'
+login_manager.login_message = 'Veuillez vous connecter pour accéder à cette page.'
+login_manager.login_message_category = 'info'
